e307c7ff1138275e24a213aeeb7bfe0f717f1561
Tyson Chen <c.tyson.0606@gmail.com>
2026-07-20 17:58:47 +0800

    Time: 0 ms (100%), Space: 8.8 MB (41.71%) - LeetHub

---
+++ 0290-word-pattern/0290-word-pattern.cpp
@@ -0,0 +1,53 @@
+class Solution {
+public:
+    bool wordPattern(string pattern, string s) {
+        unordered_map<char,string> recordPattern;
+        unordered_map<string,char> recordS;
+        int i = 0;
+        int end = 0;
+        int front = 0;
+        int count = 0;
+        while(front < s.size()){
+            if(front == s.size()-1){
+                string temp = s.substr(end,front-end+1);
+                if(recordPattern.find(pattern[i]) == recordPattern.end() && recordS.find(temp) == recordS.end()){
+                    recordPattern[pattern[i]] = temp;
+                    recordS[temp] = pattern[i];
+                }
+                else{
+                    if(recordPattern[pattern[i]] != temp || recordS[temp] != pattern[i]){
+                        return false;
+                    }
+                }
+                front++;
+                i++;
+            }
+            else if(s[front] == ' '){
+                string temp = s.substr(end,front-end);
+                if(recordPattern.find(pattern[i]) == recordPattern.end() && recordS.find(temp) == recordS.end()){
+                    recordPattern[pattern[i]] = temp;
+                    recordS[temp] = pattern[i];
+                }
+                else{
+                    if(recordPattern[pattern[i]] != temp || recordS[temp] != pattern[i]){
+                        return false;
+                    }
+                }
+                end = front + 1;
+                front += 1;
+                i++;
+                count++;
+            }
+            else{
+                front++;
+            }
+        }
+        cout << i << endl;
+        if(i != pattern.size()){
+            return false;
+        }
+        return true;
+        
+
+    }
+};
