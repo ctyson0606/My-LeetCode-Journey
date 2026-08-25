e7fa5a0e1e88d1b0bfc354ef45ac7031fd2e2143
Tyson Chen <c.tyson.0606@gmail.com>
2026-08-26 00:41:39 +0800

    Time: 0 ms (100%), Space: 25.5 MB (5.13%) - LeetHub

---
+++ 0637-average-of-levels-in-binary-tree/0637-average-of-levels-in-binary-tree.cpp
@@ -0,0 +1,48 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+private:
+    unordered_map<int,vector<int>> record;
+    int maxDepth = 0;
+public:
+    vector<double> averageOfLevels(TreeNode* root) {
+        vector<double> finalAnswer;
+        
+        GetOrder(root,0);
+        for(int i = 0;i < maxDepth+1;i++){
+            long long sum = 0;
+            for(int j = 0; j < record[i].size() ; j++){
+                sum += record[i][j];
+            }
+            double average = double(sum) / double(record[i].size());
+            finalAnswer.push_back(average);
+        }
+
+        return finalAnswer;
+    }
+
+    void GetOrder(TreeNode* root, int depth){
+        if(root != nullptr){
+            if(record.find(depth) == record.end()){
+                record[depth] = {root->val};
+            }
+            else{
+                record[depth].push_back(root->val);
+            }
+
+            if(depth > maxDepth) maxDepth = depth;
+
+            GetOrder(root->left,depth+1);
+            GetOrder(root->right,depth+1);
+        }
+    }
+};
