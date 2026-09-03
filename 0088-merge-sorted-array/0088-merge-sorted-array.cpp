15d41fe216fc0862ba4c8a9ecb6af422a264e34c
Tyson Chen <c.tyson.0606@gmail.com>
2026-09-03 21:23:41 +0800

    Time: 4 ms (1.12%), Space: 12.3 MB (96.53%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.cpp
+++ 0088-merge-sorted-array/0088-merge-sorted-array.cpp
@@ -1,9 +1,31 @@
 class Solution {
 public:
     void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
-        for(int i = m; i < m+n ; i++){
-            nums1[i] = nums2[i-m];
+        if(n == 0){
+            return;
+        } 
+        
+        int oneIdx = m-1;
+        int twoIdx = n-1;
+        
+        for(int i = m + n -1; i > -1;i--){
+            if(twoIdx < 0){
+                return;
+            }
+            else if(oneIdx < 0){
+                nums1[i] = nums2[twoIdx];
+                twoIdx--;
+            }
+            else{
+                if(nums1[oneIdx] < nums2[twoIdx]){
+                    nums1[i] = nums2[twoIdx];
+                    twoIdx--;
+                }
+                else{
+                    nums1[i] = nums1[oneIdx];
+                    oneIdx--;
+                }
+            }
         }
-        sort(nums1.begin(),nums1.end());
     }
 };
