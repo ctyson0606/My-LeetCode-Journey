0ff5d927c24f21a262d7ea4cadeae8743ca830fe
Tyson Chen <c.tyson.0606@gmail.com>
2026-09-04 20:14:41 +0800

    Time: 0 ms (100%), Space: 22.7 MB (18.56%) - LeetHub

--- 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.cpp
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.cpp
@@ -1,16 +1,17 @@
 class Solution {
 public:
     int removeDuplicates(vector<int>& nums) {
-        vector<int> copy;
-        int num = nums[0];
-        copy.push_back(nums[0]);
-        for(int i = 1; i < nums.size() ; i++){
-            if(num != nums[i]){
-                copy.push_back(nums[i]);
-                num = nums[i];
+        int size = nums.size();
+        int idx = 1;
+        int i = 1;
+        while(i < size){
+            if(nums[i] != nums[i-1]){
+                nums[idx] = nums[i];
+                idx++;
             }
+            i++;
         }
-        nums = copy;
-        return copy.size();
+
+        return idx;
     }
 };
