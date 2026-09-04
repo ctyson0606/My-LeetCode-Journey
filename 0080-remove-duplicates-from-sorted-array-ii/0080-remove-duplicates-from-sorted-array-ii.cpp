4d4e1262c894f14bf883e506b391cea0b5affed8
Tyson Chen <c.tyson.0606@gmail.com>
2026-09-04 20:48:31 +0800

    Time: 6 ms (73.91%), Space: 19.5 MB (82.18%) - LeetHub

--- 0080-remove-duplicates-from-sorted-array-ii/0080-remove-duplicates-from-sorted-array-ii.cpp
+++ 0080-remove-duplicates-from-sorted-array-ii/0080-remove-duplicates-from-sorted-array-ii.cpp
@@ -1,63 +1,20 @@
 class Solution {
 public:
     int removeDuplicates(vector<int>& nums) {
-        // Version 1
-        /*
-        int i = 1;
-        int copy = nums[0];
-        int count = 1;
-        while (i < nums.size()){
-            if(nums[i] == copy){
-                count++;
-                if(count > 2){
-                    nums.erase(nums.begin()+i);
-                }
-                else{
-                    i++;
-                }
+        int n = nums.size();
+
+        if(n <= 2) return n;
+
+        int idx = 2;
+        int i = 2;
+        while(i < n){
+            if(nums[idx-2] != nums[i]){
+                nums[idx] = nums[i];
+                idx++;
             }
-            else{
-                copy = nums[i];
-                count = 1;
-                i++;
-            }
+            i++;
         }
-        return nums.size();
-        */
 
-
-        // Version 2
-        int copy = nums[0];
-        int count = 1;
-        int repeat = -1;
-        int CountRepeat = 0;
-
-        for(int i = 1; i < nums.size() ; i++){
-            if(nums[i] == copy){
-                count++;
-                if(count > 2){
-                    CountRepeat++;
-                    if(count == 3 && repeat == -1){
-                        repeat = i;
-                    }
-                }
-                else{
-                    if(repeat != -1){
-                        nums[repeat] = copy;
-                        repeat++;  
-                    }
-                }
-            }
-            else{
-                copy = nums[i];
-                count = 1;
-                if(repeat != -1){
-                    nums[repeat] = copy;
-                    repeat++;  
-                }
-            }
-        }
-        return nums.size() - CountRepeat;
+        return idx;
     }
-    
 };
