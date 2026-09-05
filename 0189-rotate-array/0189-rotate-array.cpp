1e6a9d587df518d5ddb8a774eb61a2fe7559f07c
Tyson Chen <c.tyson.0606@gmail.com>
2026-09-05 10:03:50 +0800

    Time: 20 ms (5.34%), Space: 263.1 MB (34.85%) - LeetHub

--- 0189-rotate-array/0189-rotate-array.cpp
+++ 0189-rotate-array/0189-rotate-array.cpp
@@ -3,11 +3,12 @@ public:
     void rotate(vector<int>& nums, int k) {
         int n = nums.size();
         if(n <= 1) return;
-              
+
         vector<int> temp(n);
 
         k = k % n;
         if(k == 0) return;
+
 
         int idx = n - k;
         int i = 0;
