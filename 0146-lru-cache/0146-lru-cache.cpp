cfce1974935b9732e7616a72d250b3cd3dba1a04
Tyson Chen <c.tyson.0606@gmail.com>
2026-09-12 07:43:07 +0800

    Time: 11 ms (92.92%), Space: 50 MB (72.84%) - LeetHub

--- 0146-lru-cache/0146-lru-cache.cpp
+++ 0146-lru-cache/0146-lru-cache.cpp
@@ -1,95 +1,38 @@
 class LRUCache {
 private:
-    struct ListNode{
-        int key;
-        int val;
-        ListNode* next;
-        ListNode* pre;
-        ListNode() : key(0), val(0), next(nullptr){}
-        ListNode(int x, int y) : key(x), val(y), next(nullptr){}
-    };
-    unordered_map<int,ListNode*> record;
-    ListNode* temp;
-    ListNode* head;
-    int used = 0;
-    int size;
+  unordered_map<int, list<pair<int,int>>::iterator> record;
+  list<pair<int,int>> stored;
+  int cap;
 public:
     LRUCache(int capacity) {
-        size = capacity;
+        cap = capacity;
     }
     
     int get(int key) {
         if(record.find(key) == record.end()) return -1;
-
-        ListNode* node = record[key];
-        if(node == temp || size == 1) return node->val;
-
-        if(node == head){
-            node->next->pre = nullptr;
-            head = node->next;
-        }
-        else{
-            node->pre->next = node->next;
-            node->next->pre = node->pre;
-        }
-        temp->next = node;
-        node->pre = temp;
-        node->next = nullptr;
-        temp = node;
-        return record[key]->val;
+        auto it = record[key];
+        auto value = it->second;
+        stored.erase(it);
+        stored.push_back(pair(key,value));
+        record[key] = prev(stored.end());
+        return value;
     }
     
     void put(int key, int value) {
-        if(size != 1 || used == 0){
-            if(used >= size && record.find(key) == record.end()){
-                head->next->pre = nullptr;
-                record.erase(head->key);
-                ListNode* test = head;
-                head = head->next;
-                delete test;
-            }
-            if(used == 0){
-                ListNode* node = new ListNode(key,value);
-                record[key] = node;
-                temp = node;
-                head = temp;
-                used++;
-            }
-            else{
-                if(record.find(key) == record.end()){
-                    ListNode* node = new ListNode(key,value);
-                    record[key] = node;
-                    temp->next = node;
-                    node->pre = temp;
-                    temp = node;
-                    used++;
-                }
-                else{
-                    ListNode* node = record[key];
-                    node->val = value;
-                    if(node != temp){
-                        if(node != head){
-                            node->pre->next = node->next;
-                            node->next->pre = node->pre;
-                        }
-                        else{
-                            node->next->pre = nullptr;
-                            head = node->next;
-                        }
-                        temp->next = node;
-                        node->pre = temp;
-                        node->next = nullptr;
-                        temp = node;
-                    } 
-                }
+        if(record.find(key) != record.end()) {
+            auto it = record[key];
+            stored.erase(it);
+        } 
+        else {
+            if(stored.size() == cap){
+                auto h = stored.begin();
+                record.erase(h->first);
+                stored.pop_front();
+                
             }
         }
-        else{
-            record.erase(temp->key);
-            record[key] = temp;
-            temp->val = value;
-            temp->key = key;
-        }
+        stored.push_back(pair(key,value));
+        record[key] = prev(stored.end());
     }
 };
 
